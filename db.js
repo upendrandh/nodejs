af3e5db29779b63fc01b5cfb45c12e2063702886
upendrandh <75415386+upendrandh@users.noreply.github.com>
2021-02-12 16:05:05 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'upendra',
-  password: 'Upendra@2020',
+  password: 'upendra',
   database: 'user'
 });
 
